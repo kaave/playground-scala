585ed51a17e3cb827e73eed693fece960c056364
kaave <abe@framelunch.jp>
2019-01-14 23:28:17 +0900

    tdd: add OpenTimeRange class

---
+++ src/main/scala/OpenTimeRange.scala
@@ -0,0 +1,7 @@
+import com.github.nscala_time.time.Imports._
+
+class OpenTimeRange(val id: String, val start: DateTime, val end: DateTime)
+    extends TimeRange {
+  def contains(dt: DateTime): Boolean =
+    dt.getHourOfDay < start.getHourOfDay || end.getHourOfDay <= dt.getHourOfDay
+}
---
+++ src/test/scala/OpenTimeRangeSpec.scala
@@ -0,0 +1,17 @@
+import com.github.nscala_time.time.Imports._
+import org.scalatest._
+
+class OpenTimeRangeSpec extends FunSpec with DiagrammedAssertions {
+  describe("contains") {
+    it("時間帯に含むか") {
+      val range = new OpenTimeRange("",
+                                    DateTime.parse("2018-01-01T05:00:00"),
+                                    DateTime.parse("2018-01-01T12:00:00"))
+      assert(range.contains(DateTime.parse("2018-01-01T04:00:00")))
+      assert(!range.contains(DateTime.parse("2018-01-01T05:00:00")))
+      assert(!range.contains(DateTime.parse("2018-01-01T10:00:00")))
+      assert(range.contains(DateTime.parse("2018-01-01T12:00:00")))
+      assert(range.contains(DateTime.parse("2018-01-01T20:00:00")))
+    }
+  }
+}
