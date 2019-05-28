b81dfdf13dfc0fe6b308c0995b78b479ee45f1f3
kaave <junkjunctions@gmail.com>
2019-05-28 23:12:30 +0900

    finish: 2.5

--- src/main/scala/fp_in_scala/gettingstarted/GettingStarted.scala
+++ src/main/scala/fp_in_scala/gettingstarted/GettingStarted.scala
@@ -10,6 +10,19 @@ object MyModule {
     go(n, 1)
   }
 
+  def fib(count: Int): Int = {
+    def go(ary: Array[Int], currentCount: Int): Int = {
+      val sum = ary(0) + ary(1)
+
+      if (currentCount >= count)
+        sum
+      else
+        go(sum +: ary, currentCount + 1)
+    }
+
+    go(Array(1, 0), 2)
+  }
+
   private def formatAbs(x: Int): String = {
     val msg = "The absolute value of %d is %d"
     msg.format(x, abs(x))
@@ -25,25 +38,38 @@ object MyModule {
   }
 
   /*
-   * practice
+   * 2.5
    */
-  def fib(count: Int): Int = {
-    def go(ary: Array[Int], currentCount: Int): Int = {
-      val sum = ary(0) + ary(1)
-
-      if (currentCount >= count)
-        sum
+  def findFirst[A](as: Array[A], p: A => Boolean): Int = {
+    @annotation.tailrec
+    def loop(n: Int): Int =
+      if (n >= as.length)
+        -1
+      else if (p(as(n)))
+        n
       else
-        go(sum +: ary, currentCount + 1)
-    }
+        loop(n + 1)
 
-    go(Array(1, 0), 2)
+    loop(0)
+  }
+
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    def loop(i: Int): Boolean =
+      if (i >= as.length)
+        true
+      else if (!ordered(as(i), as(i - 1)))
+        false
+      else
+        loop(i + 1)
+
+    loop(1)
   }
 
 //  def main(args: Array[String]): Unit = println(factorial(2))
 //  def main(args: Array[String]): Unit = println(fib(10))
   def main(args: Array[String]): Unit = {
-    println(formatResult("absolute value", -42, abs))
-    println(formatResult("factorial value", 7, factorial))
+//    println(formatResult("absolute value", -42, abs))
+//    println(formatResult("factorial value", 7, factorial))
+    println(isSorted(Array(1, 3, 7, 5, 9), (i: Int, j: Int) => i > j))
   }
 }
